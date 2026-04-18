e9eac3d824e172ef7d8c9e68c91eb235226f90db
Boutachrafine <boutachrafine.nabil@gmail.com>
2026-04-18 10:47:29 +0100

    add compose.yml and commands

--- src/main/java/ma/bout/kafkaspringcloudstream/KafkaSpringCloudStreamApplication.java
+++ src/main/java/ma/bout/kafkaspringcloudstream/KafkaSpringCloudStreamApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaSpringCloudStreamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaSpringCloudStreamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringCloudStreamApplication.class, args);
+    }
 
 }
